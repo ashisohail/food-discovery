adc39240302387ab91f9571831c4595682feff54
Ayesha <pu_ash@yahoo.com>
2024-02-20 10:39:12 +0000

    Completed login and registration backend functionality for user.

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import { userRouter } from "./src/routes/users.js";
 
 const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
@@ -19,12 +20,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = 8080;
+app.use("/auth", userRouter);
+
+const port = 3001;
 
 //Get all Method
-app.get("/", (req, res) => {
-  res.send("I am ayesha");
-});
+// app.get("/", (req, res) => {
+//   res.send("I am ayesha");
+// });
 
 app.listen(port, () => {
   console.log(`Server Started at ${port}`);
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,1 +1,61 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/users.js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  if (user) {
+    return res.json({ message: "User already exists!" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+
+  res.json({ message: "User Registered Successfully!" });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "User Doesn't Exist! " });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or Password Is Incorrect!" });
+  }
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user._id });
+});
+
+// router.post("/register", async (req, res) => {
+//   const { username, password } = req.body;
+
+//   try {
+//     const user = await UserModel.findOne({ username });
+//     res.status(200).json(user);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// });
+
+//Get all Method
+// router.get("/getAll", async (req, res) => {
+//   try {
+//     const user = await UserModel.find();
+//     res.json(user);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+export { router as userRouter };
